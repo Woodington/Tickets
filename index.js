694743828844d8042b73ca381a8463812cc7de2a
Woodington <93148433+Woodington@users.noreply.github.com>
2021-11-15 18:41:41 +0000

    Bulk Update:
    1) Added config file 
    2) Put all message functions in one messageCreate, instead of two
    3) Changed Accept/Reject DM from Plain Text to Embed  
    4) Made code extremely easy to read with comments
    5) Tidied Up Code

--- index.js
+++ index.js
@@ -2,54 +2,54 @@ const Discord = require("discord.js")
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [30211] }); 
 
-const prefix = "-" 
+const config = require('./config/config.json') //Declars Config File
 
+const token = config.token; //Decalares Token
 
-const fs = require("fs");
+const fs = require("fs"); //Declares Filesystem 
 
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js")) 
-client.commands = new Discord.Collection();  
-for(const file of commandFiles) { 
-    const command = require(`./commands/${file}`);
+const prefix = config.prefix; //Prefix for the accept/reject commands
 
-    client.commands.set(command.name, command);
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))  //Lays down framework for command handler
+client.commands = new Discord.Collection(); //Lays down framework for command handler
+for(const file of commandFiles) { //Lays down framework for command handler
+    const command = require(`./commands/${file}`); //Lays down framework for command handler
+
+    client.commands.set(command.name, command); //Lays down framework for command handler
 }
 
-let myChannel = '909236971065442345' 
-let ticketChannel = '909236956309884998'
+let targetChannel = config.targetChannel //Sets Target Channel
+let ticketChannel = config.ticketChannel //Sets Ticket Channel
 
-client.once('ready', () => { 
-    console.log('Ready! - Made By Woodington');
-});  
+client.once('ready', () => {  //Sends a message to console when the bot starts
+    console.log('Ready! - Made By Woodington'); //Sends a message to console when the bot starts
+});  //Sends a message to console when the bot starts
 
-client.on('messageCreate', gotMessage);
-function gotMessage(msg) { 
-    if (msg.channel.id === ticketChannel) { 
-        client.channels.fetch(myChannel)
-           .then(channel => channel.send(msg.content + `\n**Ticket ID:** ${msg.author.id}\n` + `**Mention:** <@${msg.author.id}>`))
-           .catch(console.error)
-    }
-};
+client.on('messageCreate', gotMessage); //Message Forwarding Function
+function gotMessage(msg) { //Message Forwarding Function
+    if (msg.channel.id === ticketChannel) { //Message Forwarding Function
+        client.channels.fetch(targetChannel) //Message Forwarding Function
+           .then(channel => channel.send(msg.content + `\n**Ticket ID:** ${msg.author.id}\n` + `**Mention:** <@${msg.author.id}>`)) //Message Forwarding Function
+           .catch(console.error) //Catches Errors with message Forwarding
+    } //Message Forwarding Function
+}; //Message Forwarding Function
 
-client.on("messageCreate", (message) => { 
-    if(message.channel.id == "909236956309884998"){
-        message.delete()
-    }
-});   
+client.on("messageCreate", (message) => {  //Deletes Messages when sent
+    if(message.channel.id == ticketChannel){ //Sets channel for message deletion 
+        message.delete() //Deletes Messages when sent
+    } //Deletes Messages when sent
 
-
-client.on('messageCreate', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return; 
+    if(!message.content.startsWith(prefix) || message.author.bot) return; //Checks if 1) Command doesnt start with prefix, 2) If the command is sent by a bot. If either are true will not go through
 
     const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase()
+    const command = args.shift().toLowerCase() //Makes the command not case senstive
 
-    if(command === "accept") {
-        client.commands.get("accept").run(client, message, args);
-    } 
-    if(command === "reject") {
-        client.commands.get("reject").run(client, message, args);
-    }
-});
+    if(command === "accept") { //accept command
+        client.commands.get("accept").run(client, message, args); //accept command
+    } //accept command
+    if(command === "reject") { //reject command
+        client.commands.get("reject").run(client, message, args); //reject command
+    } //reject command
+});   
 
-client.login("OTA3Mzc2OTYzNTYyNDAxODIy.YYmSpg.UaO9U2Z-omjHevhL_Q3eKaXsCls"); 
+client.login(token); 
