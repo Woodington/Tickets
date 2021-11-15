d9db4b9a62c752a08b4f0a0430849e8adef96d08
Woodington <93148433+Woodington@users.noreply.github.com>
2021-11-15 18:42:15 +0000

    Bulk Update
    1) Added config file 
    2) Put all message functions in one messageCreate, instead of two
    3) Changed Accept/Reject DM from Plain Text to Embed
    4) Made code extremely easy to read with comments
    5) Tidied Up Code

--- commands/accept.js
+++ commands/accept.js
@@ -1,17 +1,23 @@
-const {Client, Message, MessageEmbed} = require('discord.js');
+const {Client, Message, MessageEmbed} = require('discord.js'); 
+const fs = require('fs'); 
+const config = require('../config/config.json')
 
 module.exports = {
-    name: 'accept', 
+    name: 'accept', //sets command name
     /**
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args
      */ 
     run: async(client, message, args) => { 
-        if(!message.member.permissions.has('MANAGE_MESSAGES')) return; 
+        if(!message.member.permissions.has('MANAGE_MESSAGES')) return; //Checks if user has delete messages permission 
 
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user 
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user  //Sets the user to the first metioned person
 
-        user.send(`Ticket Accepted By: **${message.author.tag}**`)
+        const acceptEmbed = new MessageEmbed() //Sets up the embed
+        .setColor(config.acceptColor) //Sets Embed Color
+        .setDescription(`Ticket Accepted By: **${message.author.tag}**`) //Embed text
+
+        user.send({ embeds: [acceptEmbed] }) //Sends the DM
         }
     };
