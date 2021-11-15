a5346dd73f03867aeeceeab12d8e5aac7147fe34
Woodington <93148433+Woodington@users.noreply.github.com>
2021-11-15 18:43:28 +0000

    Bulk Update
    1) Added config file 
    2) Put all message functions in one messageCreate, instead of two
    3) Changed Accept/Reject DM from Plain Text to Embed
    4) Made code extremely easy to read with comments
    5) Tidied Up Code

--- commands/reject.js
+++ commands/reject.js
@@ -1,19 +1,27 @@
-const {Client, Message, MessageEmbed} = require('discord.js');
+const {Client, Message, MessageEmbed} = require('discord.js'); 
+const fs = require('fs'); 
+const config = require('../config/config.json')
 
 module.exports = {
-    name: 'reject', 
+    name: 'reject', //sets command name
     /**
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args
-     */ 
-    run: async(client, message, args) => { 
-        if(!message.member.permissions.has('MANAGE_MESSAGES')) return; 
+     */  
 
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user  
+    run: async(client, message, args) => {  
 
-        const str = args.slice(1).join(' ')
+        if(!message.member.permissions.has('MANAGE_MESSAGES')) return; //Checks if user has delete messages permission 
 
-        user.send(`Ticket Rejected\n**Reason:** ${str}`)
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user //Sets the user to the first metioned person
+
+        const str = args.slice(1).join(' ') //Removes the user ID from the DM 
+
+        const rejectEmbed = new MessageEmbed() //Sets up the embed
+        .setColor(config.rejectColor)   //Sets embed color
+        .setDescription(`Ticket Rejected\n**Reason:** ${str}`) //sets embed message
+
+        user.send({ embeds: [rejectEmbed] }) //Sends the DM
         }
     };
