3d41ee03647f275b0026a9f3994853fb564eb6d0
Woodington <93148433+Woodington@users.noreply.github.com>
2022-02-09 22:55:26 +0000

    Tidied up comments

--- commands/accept.js
+++ commands/accept.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const config = require('../config/config.json')
 
 module.exports = {
-    name: 'accept', //sets command name
+    name: 'accept', 
     /**
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args
      */ 
     run: async(client, message, args) => { 
-        if(!message.member.permissions.has('MANAGE_MESSAGES')) return; //Checks if user has delete messages permission 
+        if(!message.member.permissions.has('MANAGE_MESSAGES')) return; 
 
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user  //Sets the user to the first metioned person
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user  
 
-        const acceptEmbed = new MessageEmbed() //Sets up the embed
-        .setColor(config.acceptColor) //Sets Embed Color
-        .setDescription(`Ticket Accepted By: <@${message.author.id}>`) //Embed text
+        const acceptEmbed = new MessageEmbed() 
+        .setColor(config.acceptColor) 
+        .setDescription(`Ticket Accepted By: <@${message.author.id}>`) 
 
-        user.send({ embeds: [acceptEmbed] }) //Sends the DM
+        user.send({ embeds: [acceptEmbed] }) 
         }
     };
